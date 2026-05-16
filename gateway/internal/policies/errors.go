076e7e5d8d1e5077ef5cd87ab090d405f64fa4b4
TheKazuto <watarizangado@gmail.com>
2026-05-16 07:43:25 -0300

    Delete gateway/internal/policies directory

--- gateway/internal/policies/errors.go
+++
@@ -1,8 +0,0 @@
-package policies
-
-import "errors"
-
-var (
-	ErrUnknownTemplate     = errors.New("unknown template")
-	ErrTemplateNotDeployed = errors.New("template not deployed (no program id configured)")
-)
